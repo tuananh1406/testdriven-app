9cf9bb5e5a5c111a6046e77f7dc50d04a53e1587
Hữu Tuấn Anh <huutuananh1406@gmail.com>
2021-02-18 14:50:57 +0700

    Xong phần Testing React, trang 93. Tạm dừng...

--- services/client/cai_dat_react.sh
+++ services/client/cai_dat_react.sh
@@ -5,3 +5,5 @@ sudo apt install nodejs npm -y
 # Cài đặt các gói cần thiết
 npm install create-react-app --global
 npm install axios --save
+npm install --save-dev enzyme enzyme-adapter-react-16
+npm install react-scripts --global
